9488a9cdc62b053b3e244c4dd2be2d9ef923f7ae
afonsoleite21-source <afonso.leite21@gmail.com>
2026-03-19 09:34:13 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("site a funcionar");
+fetch('products.json')
+  .then(res => res.json())
+  .then(data => {
+    const container = document.getElementById('products');
+
+    data.forEach(product => {
+      container.innerHTML += `
+        <div class="card">
+          <img src="${product.image}">
+          <h3>${product.name}</h3>
+          <p class="price">€${product.price}</p>
+          <p class="old-price">€${product.oldPrice}</p>
+          <button onclick="addToFav('${product.name}')">❤️ Favorito</button>
+        </div>
+      `;
+    });
+  });
